e5e7cc984a2a85947d321c361e33e39ae488aed8
Jersh-dev <joshuablankenship68@gmail.com>
2025-07-13 21:38:54 -0600

    B: Added a clock and dt variable

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import pygame
 from constants import *
 
 def main():
+    #starts pygame
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Opens the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #makes a clock to help with resource load
+    clock = pygame.time.Clock() 
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +26,9 @@ def main():
         
         screen.fill((0,0,0))
         pygame.display.flip()
+        
+        #limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
